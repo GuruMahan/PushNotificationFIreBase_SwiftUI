a907db1dffb71abeca2ea2940ca1ce53c652638b
GuruMahan <iosdeve2022@gmail.com>
2023-02-10 18:42:02 +0530

    Initial Commit

---
+++ PushNotificationFIreBasePOC/PushNotificationFIreBasePOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushNotificationFIreBasePOCApp.swift
+//  PushNotificationFIreBasePOC
+//
+//  Created by Guru Mahan on 10/02/23.
+//
+
+import SwiftUI
+
+@main
+struct PushNotificationFIreBasePOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
